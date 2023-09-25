a1b9bc3131788e2bab2a61393f2876f189e37a19
Abhimanyu Kumar <kabhimanyu656@gmail.com>
2023-09-25 10:06:29 +0530

    Starting from scratch

--- app.js
+++ app.js
@@ -21,18 +21,22 @@
 // console.log(heading); //Object
 // import React from "react";
 // import { ReactDOM } from "react-dom/client";
-import React from "react";
-import ReactDOM from "react-dom";
 
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child" }, [
-    React.createElement("h1", {}, "I'm an H1 Tag"),
-    React.createElement("h2", {}, "I'm an H2 tag"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I'm an H1 Tag"),
-    React.createElement("h2", {}, "I'm an H2 tag"),
-  ]),
-]);
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+// const parent = React.createElement("div", { id: "parent" }, [
+//   React.createElement("div", { id: "child" }, [
+//     React.createElement("h1", {}, "I'm an H1 Tag"),
+//     React.createElement("h2", {}, "I'm an H2 tag"),
+//   ]),
+//   React.createElement("div", { id: "child2" }, [
+//     React.createElement("h1", {}, "I'm an H1 Tag"),
+//     React.createElement("h2", {}, "I'm an H2 tag"),
+//   ]),
+// ]);
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(parent);
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+
